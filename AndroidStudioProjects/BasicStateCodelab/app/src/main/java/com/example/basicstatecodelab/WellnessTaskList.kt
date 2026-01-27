e585eaf6cf3889b8bf20aaced044a46d9ffe491d
Benjamin <benjiAndroidDev@users.noreply.github.com>
2026-01-27 19:38:26 +0100

    Initial commit

--- AndroidStudioProjects/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessTaskList.kt
+++ AndroidStudioProjects/BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessTaskList.kt
@@ -1,2 +1,26 @@
 package com.example.basicstatecodelab
 
+import WellnessTaskItem
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.Column
+fun getWellnessTask() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
+
+@Composable
+fun WellnessTaskList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getWellnessTask() }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(list) { task ->
+            WellnessTaskItem(taskName = task.label)
+        }
+    }
+}
+
